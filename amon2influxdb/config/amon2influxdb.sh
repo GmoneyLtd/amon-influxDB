0fe9d71b318fa3f80d80ade2840a52598cd8f130
apuer <apuer110@gmail.com>
2022-07-22 23:47:35 +0800

    change amon2influxdb service

--- amon2influxdb/config/amon2influxdb.sh
+++ amon2influxdb/config/amon2influxdb.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo $(pwd)
-git  clone -b v1.0 https://github.com/GmoneyLtd/amon_Consumed_influxdb.git  && pip install --upgrade pip && pip install -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com -r requirements.txt >>./log/amon2influxdb_$(date "+%Y-%m-%d").log 2>&1
-nohup python ./amon_Consumed_influxdb/setup.py & 
+git  pull  >>./log/amon2influxdb_$(date "+%Y-%m-%d").log 2>&1
+nohup python ./amon_Consumed_influxdb/setup.py >>./log/amon2influxdb_$(date "+%Y-%m-%d").log 2>&1 &
 echo "amon2influxdb is running"
-echo "please check the log file"
